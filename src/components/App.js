da911c3cb063a54dfb30adba2ab20e1c863705ff
Laurine Maina <mainalaureen58@gmail.com>
2024-04-23 14:52:24 +0300

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -4,28 +4,43 @@ function NavBar() {
   return (
     <nav>
       <a href="#home">I'm a link!</a>
+      <a href="#about"></a>
     </nav>
   );
 }
 
 function Home() {
   return (
-    <div id="home">
-      <h1>Home</h1>
-    </div>
+      <div id="home">
+        <h1>Home</h1>
+      </div>
   );
 }
 
-{/* write an <About> component here */}
+function About(){
+  return(
+  <div id="about">
+  <h2>About</h2>
+</div>
+  )
+}
+
+
 
 
 function App() {
   return (
     <div>
-      <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
-    </div>
+        <div>
+        <NavBar />
+        <Home />
+        </div>
+        <div>
+          <About />
+          </div>
+      </div>
+    
+    
   );
 }
 
